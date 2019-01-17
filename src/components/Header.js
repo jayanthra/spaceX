4fe44695cfef20319c69d24c9477f03fedf02f98
Jayanth <jayanthacharya4@gmail.com>
2019-01-17 23:00:02 +0530

    ui changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-
 class Header extends Component {
    
     render() {
         return (
             <div className="header">          
-                <img src="https://www.spacex.com/sites/all/themes/spacex2012/logo.png" className="logo-image" alt="logo"></img>
+                <img src={require('../assets/logo.png')} className="logo-image" alt="logo"></img>
             </div>
         );
     }
--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -18,22 +18,51 @@ class TimelineItem extends Component {
             background: this.props.event.id === 1 ? 'white' : 'black'
         }
     }
-    render() {
 
+    dateConvert(part) {
+        const date = new Date(this.props.event.event_date_unix * 1000);
+        let datePart;
+        if (part === "year")
+            datePart = date.getFullYear();
+        if (part === "month")
+            datePart = date.getMonth();
+        if (part === "day")
+            datePart = date.getDate();
+        return datePart;
+    }
+
+   
+
+    getBGColor(index){      
+        const colorSet = ["#b2dc33","#f1ce02","#ff79ac","#bcb6ff"]
+
+        let hexVal = colorSet[index%4]
+
+        return {
+            background: hexVal
+        }
+    }
+
+    render() {
         return (
             <div className="timeline-item">
                 <div className="column-1">
-                    <div className="holder">
-                        <p>{this.props.event.title}</p>
+                    <div className="holder" style={this.getBGColor(this.props.event.id)}>
+                        <h3>{this.props.event.title}</h3>
                     </div>
                 </div>
                 <div className="column-2">
                     <div className="line" style={this.getStyle()}></div>
-                    <div className="circle"></div>
+                    <div className="circle">
+                        <div className="date-holder">                           
+                            <p>{this.dateConvert("day")}-{this.dateConvert("month")}</p>
+                            <p>{this.dateConvert("year")}</p>
+                        </div>
+                    </div>
                     <div className="line"></div>
                 </div>
                 <div className="column-3">
-                    <div className="holder">
+                    <div className="holder" style={this.getBGColor(this.props.event.id+1)}>
                         <p>{this.props.event.details}</p>
                     </div>
                 </div>
